27f36be8456f5200420ff704db753ea7a6545689
nadaeezzdin <nadaeezzdin@gmail.com>
2025-01-03 04:20:32 +0200

    add find user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
 export class UsersController {
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+ constructor(protected prismaService: PrismaService){} 
+ 
+ async findOne(id: number) {
+   return this.prismaService.user.findUnique({
+     where: { id }
+   });
+ }
+}
