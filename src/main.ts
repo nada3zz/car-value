a34b66f4166a1883851dd55b350a29ba3e536764
nadaeezzdin <nadaeezzdin@gmail.com>
2025-01-10 23:51:58 +0200

    apply global interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { setupSession } from './shared/config';
 import { setupSecurityHeaders } from './shared/middlewares';
 import { disableETags } from './shared/config';
+import { CurrentUserInterceptor } from './modules/auth/interceptor';
+import { UsersService } from './modules/users/users.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +17,7 @@ async function bootstrap() {
   setupSession(app);
   setupSecurityHeaders(app);
   disableETags(app);
+  app.useGlobalInterceptors(new CurrentUserInterceptor(app.get(UsersService)));
 
   const port = process.env.PORT || 5000;
   await app.listen(port);
