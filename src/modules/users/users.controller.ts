a96729da3c19705650d0e404ccf2517cebc35e7d
nadaeezzdin <nadaeezzdin@gmail.com>
2025-01-10 23:51:17 +0200

    add user services

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Session, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { CurrentUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
-@Controller('auth')
+@Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    console.log(user);
+    return user;
+  }
 
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,17 +3,30 @@ import { PrismaService } from 'src/shared';
 
 @Injectable()
 export class UsersService {
- constructor(protected prismaService: PrismaService){} 
- 
- async findById(id: number) {
-   return this.prismaService.user.findUnique({
-     where: { id }
-   });
- }
+  constructor(protected prismaService: PrismaService) {}
 
- async findBySessionId(sessionId: string) {
-    return this.prismaService.user.findUnique({
-      where: { sessionId }
+  async findById(id: number) {
+    return await this.prismaService.user.findUnique({
+      where: { id },
     });
- }
+  }
+
+  async findBySessionId(sessionId: string) {
+    return await this.prismaService.user.findUnique({
+      where: { sessionId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        reports: true,
+      }
+    });
+  }
+
+  async Delete(id: number) {
+    return await this.prismaService.user.delete({
+      where: { id },
+    });
+  }
 }
