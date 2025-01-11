58dbf7bb15ef77f3ead1efe54880fc64a077abf1
nadaeezzdin <nadaeezzdin@gmail.com>
2025-01-11 02:28:15 +0200

    add reports controller

--- src/modules/reports/reports.controller.ts
+++ src/modules/reports/reports.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Post, Body, Patch, UseGuards, Param } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { ApproveReportDto, CreateReportDto, GetEstimateDto } from './dtos';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { CurrentUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
 @Controller('reports')
 export class ReportsController {
-    constructor( private readonly ReportsService: ReportsService){}
+    constructor( private reportsService: ReportsService){}
 
+    @Get('/estimate')
+    async getEstimate(@Query() query: GetEstimateDto) {
+      return await this.reportsService.getEstimate(query);
+    }
+  
+    @Post()
+    @UseGuards(AuthGuard)
+    async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+      return await this.reportsService.create(body, user.id);
+    }
+
+    @Get()
+    @UseGuards(AuthGuard)
+    async getAllReports() {
+      return await this.reportsService.getAll();
+    }
+
+    @Get('/:id')
+    @UseGuards(AuthGuard)
+    async getReport(@Param('id') id: string) {
+      return await this.reportsService.getOne(parseInt(id));
+    }
+
+    @Patch('/:id')
+    @UseGuards(AuthGuard)
+    async approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+      return await this.reportsService.changeApproval(parseInt(id), body.approved);
+    }
 
 }
